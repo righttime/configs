610f1e237f88300a32ea51a080426f5e7faf5dfb
righttime <eyedoloveu@gmail.com>
2024-05-10 16:05:12 +0900

    almost Like it

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,16 @@
-oh-my-posh init pwsh | Invoke-Expression
-oh-my-posh init pwsh --config 'D:\Git\configs\Windows\hul10.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'D:\Git\configs\Windows\iterm2.omp.json' | Invoke-Expression
+
+Import-Module PSColor
+Import-Module posh-git
+Import-Module git-aliases -DisableNameChecking
+Import-Module PSReadLine
+
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
+Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineOption -Colors @{ InlinePrediction = "#267233" }
