1d6f7025e3741e3754687afd4bc48d7fabaaffda
righttime <eyedoloveu@gmail.com>
2024-05-11 23:10:54 +0900

    added function

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,19 @@
 oh-my-posh init pwsh --config 'D:\Git\configs\Windows\iterm2.omp.json' | Invoke-Expression
 
-Import-Module PSColor
-Import-Module posh-git
-Import-Module git-aliases -DisableNameChecking
+#Import-Module PSColor
+#Import-Module posh-git
+#Import-Module git-aliases -DisableNameChecking
 Import-Module PSReadLine
 
-
+#PSReadline settings
+Set-PSReadLineOption -PredictionViewStyle InlineView #ListView
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
-Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
-Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
+Set-PSReadLineOption -Colors @{emphasis = '#99aaff'; inlinePrediction = '267233'}
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadLineOption -Colors @{ InlinePrediction = "#267233" }
+
+Function goWork{
+    cd D:\Git
+}
